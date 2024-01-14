103bacfa583f025c1cd89dbed2fe4e60eb2b84b3
mazenhesham17 <mazenhesham7@live.com>
2024-01-14 03:11:24 +0200

    finish styling footer component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,30 +1,33 @@
 import logo from "../assets/Asset 9@4x.png"
+import style from "../styles/footer.module.css"
 
-function Footer(){
+function Footer() {
     return (
-        <>
-        <img src={logo} alt="little lemon cropped logo"/>
-        <>
-        <h1>Links</h1>
-        <ul>
-            <li><a>Home</a></li>
-            <li><a>About</a></li>
-            <li><a>Menu</a></li>
-            <li><a>Reservations</a></li>
-            <li><a>Order online</a></li>
-            <li><a>Login</a></li>
-        </ul>
-        </>
-        <>
-        <h1>Contact us</h1>
-        <ul>
-            <li><a>Address</a></li>
-            <li><a>Phone number</a></li>
-            <li><a>Email Address</a></li>
-            <li><a>Facebook</a></li>
-        </ul>
-        </>
-        </>
+        <div className={style.section} >
+            <div className={style.footer}>
+                <img src={logo} alt="little lemon cropped logo" height={200} width={200} />
+                <div>
+                    <h1 className={style.heading1}>Links</h1>
+                    <ul className={style.list}>
+                        <li className={style.list_item}><a>Home</a></li>
+                        <li className={style.list_item}><a>About</a></li>
+                        <li className={style.list_item}><a>Menu</a></li>
+                        <li className={style.list_item}><a>Reservations</a></li>
+                        <li className={style.list_item}><a>Order online</a></li>
+                        <li className={style.list_item}><a>Login</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h1 className={style.heading1}>Contact us</h1>
+                    <ul className={style.list}>
+                        <li className={style.list_item}><a>Address</a></li>
+                        <li className={style.list_item}><a>Phone number</a></li>
+                        <li className={style.list_item}><a>Email Address</a></li>
+                        <li className={style.list_item}><a>Facebook</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     );
 }
 
