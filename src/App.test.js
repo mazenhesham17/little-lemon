36b03796bd3f119db07f3f62363b6f7cdb178164
mazenhesham17 <mazenhesham7@live.com>
2024-01-17 20:43:43 +0200

    add unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,33 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { HashRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders Hero section', () => {
+  render(
+    <HashRouter>
+      <App />
+    </HashRouter>
+  );
+  const linkElement = screen.getByText(/We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist./i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Specials section', () => {
+  render(
+    <HashRouter>
+      <App />
+    </HashRouter>
+  );
+  const linkElement = screen.getByText(/Specials/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders Testimonials section', () => {
+  render(
+    <HashRouter>
+      <App />
+    </HashRouter>
+  );
+  const linkElement = screen.getByText(/Testimonials/i);
   expect(linkElement).toBeInTheDocument();
 });
